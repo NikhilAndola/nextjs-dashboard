fea4c27b9b6fb213a74a2b78e2bd3d8b50e3e64e
NikhilAndola <nikhilandola123@gmail.com>
2024-02-22 19:08:09 +0530

    done changing

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -117,18 +117,18 @@ export async function deleteInvoice(id: string) {
 export async function authenticate(
     prevState: string | undefined,
     formData: FormData,
-  ) {
+) {
     try {
-      await signIn('credentials', formData);
+        await signIn('credentials', formData);
     } catch (error) {
-      if (error instanceof AuthError) {
-        switch (error.type) {
-          case 'CredentialsSignin':
-            return 'Invalid credentials.';
-          default:
-            return 'Something went wrong.';
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
         }
-      }
-      throw error;
+        throw error;
     }
-  }
+}
